1aab217fd9b6d2d0444c5ed3bbfaae45e8cd3f2b
Bae Hyeonseo <haulqogustj@naver.com>
2024-05-08 03:36:51 +0900

    [Main] #31 application-SyncManageService

--- Main/src/main/java/com/kusitms29/backendH/domain/sync/application/service/SyncManageService.java
+++ Main/src/main/java/com/kusitms29/backendH/domain/sync/application/service/SyncManageService.java
@@ -5,6 +5,9 @@ import com.kusitms29.backendH.domain.sync.domain.Sync;
 import com.kusitms29.backendH.domain.sync.domain.service.SyncReader;
 import com.kusitms29.backendH.domain.user.domain.User;
 import com.kusitms29.backendH.domain.user.domain.UserCategory;
+import com.kusitms29.backendH.domain.user.domain.service.UserCategoryManager;
+import com.kusitms29.backendH.domain.user.domain.service.UserCategoryReader;
+import com.kusitms29.backendH.domain.user.domain.service.UserReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +17,18 @@ import java.util.List;
 public class SyncManageService {
     private final SyncReader syncReader;
     private final UserReader userReader;
-    private final UserCategory userCategory;
-    private final SyncCategoryReader syncCategoryReader;
+    private final UserCategoryReader userCategoryReader;
+    private final UserCategoryManager userCategoryManager;
     private final ParticiationManager particiationManager;
     public List<SyncInfoResponseDto> recommendSync(Long userId){
         User user = userReader.findByUserId(userId);
-        List<UserCategory> userCategories = findAllByUserId(userId);
-        List<String> categories = getTypeByCategories(userCategories);
-        List<Sync> syncList = syncReader.findByTypeWithInterestWithLocation(user.getSyncType(), categories, user.getLocation);
+        List<UserCategory> userCategories = userCategoryReader.findAllByUserId(userId);
+        List<String> categories = userCategoryManager.getTypeByUserCategories(userCategories);
+        List<Sync> syncList = syncReader.findBySyncTypeWithInterestWithLocation(user.getSyncType(), categories, user.getLocation);
         return syncList.stream().map( sync -> SyncInfoResponseDto.of(
                 sync.getId(),
                 sync.getSyncType(),
-                syncCategoryReader.findBySyncId().getCategory.getType,
+                sync.getType(),
                 sync.getImage(),
                 particiationManager.countParticipationBySyncId(sync.getId()),
                 sync.getMember_max(),
