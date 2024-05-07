aa310a986f4f5b7392bae5ade169ec9c3650a976
Bae Hyeonseo <haulqogustj@naver.com>
2024-05-08 04:10:31 +0900

    [Main] #31 SyncController

--- Main/src/main/java/com/kusitms29/backendH/domain/sync/application/controller/SyncManageController.java
+++ Main/src/main/java/com/kusitms29/backendH/domain/sync/application/controller/SyncManageController.java
@@ -1,11 +1,25 @@
 package com.kusitms29.backendH.domain.sync.application.controller;
 
+import com.kusitms29.backendH.domain.sync.application.controller.dto.response.SyncInfoResponseDto;
+import com.kusitms29.backendH.domain.sync.application.service.SyncManageService;
+import com.kusitms29.backendH.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/sync")
 @RestController
 public class SyncManageController {
+    private final SyncManageService syncManageService;
+    @GetMapping
+    public ResponseEntity<SuccessResponse<?>> recommendSync(@RequestParam(name = "userId") Long userId) {
+        List<SyncInfoResponseDto> syncInfoResponseDtos = syncManageService.recommendSync(userId);
+        return SuccessResponse.ok(syncInfoResponseDtos);
+    }
 }
