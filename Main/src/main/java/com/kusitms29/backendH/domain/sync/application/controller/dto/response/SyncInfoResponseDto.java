228efe88073740ce6d47e2b651753ad73f8dd3da
Bae Hyeonseo <haulqogustj@naver.com>
2024-05-08 03:50:25 +0900

    [Main] #31 dto 수정

--- Main/src/main/java/com/kusitms29/backendH/domain/sync/application/controller/dto/response/SyncInfoResponseDto.java
+++ Main/src/main/java/com/kusitms29/backendH/domain/sync/application/controller/dto/response/SyncInfoResponseDto.java
@@ -1,5 +1,11 @@
 package com.kusitms29.backendH.domain.sync.application.controller.dto.response;
 
+import com.kusitms29.backendH.domain.category.domain.Type;
+import com.kusitms29.backendH.domain.sync.domain.SyncType;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public record SyncInfoResponseDto(
         Long syncId,
         String type,
@@ -11,15 +17,18 @@ public record SyncInfoResponseDto(
         String location,
         String date
 ) {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("M월 d일 (E) a h:mm");
+
     public static SyncInfoResponseDto of(Long syncId,
-                                         String type,
-                                         String interest,
+                                         SyncType syncType,
+                                         Type type,
                                          String image,
                                          int userCnt,
                                          int totalCnt,
                                          String syncName,
                                          String location,
-                                         String date){
-        return new SyncInfoResponseDto(syncId,type,interest,image,userCnt,totalCnt,syncName,location,date);
+                                         LocalDateTime date){
+        String formattedDate = date.format(DATE_TIME_FORMATTER);
+        return new SyncInfoResponseDto(syncId, String.valueOf(syncType), String.valueOf(type), image, userCnt, totalCnt, syncName, location, formattedDate);
     }
 }
