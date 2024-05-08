30d92a7888562d26e3eaadf7286bfb984b5c516a
spacewalk00 <ju001217@gmail.com>
2024-05-08 22:40:27 +0900

    [Main]: #34 국가 조회하기 api 구현

--- Main/src/main/java/com/kusitms29/backendH/domain/user/application/controller/UserController.java
+++ Main/src/main/java/com/kusitms29/backendH/domain/user/application/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.kusitms29.backendH.domain.user.application.controller;
 
 
+import com.kusitms29.backendH.domain.user.application.controller.dto.request.CountryCalloutRequestDto;
 import com.kusitms29.backendH.domain.user.application.controller.dto.request.UserSignInRequestDto;
 import com.kusitms29.backendH.domain.user.application.controller.dto.response.UserAuthResponseDto;
 import com.kusitms29.backendH.domain.user.application.service.AuthService;
+import com.kusitms29.backendH.domain.user.application.service.CountryDataService;
 import com.kusitms29.backendH.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 @RestController
 public class UserController {
     private final AuthService authService;
+    private final CountryDataService countryDataService;
 
     @PostMapping("/signin")
     public ResponseEntity<SuccessResponse<?>> signIn(@RequestHeader("Authorization") final String authToken,
@@ -21,6 +26,11 @@ public class UserController {
                                                      @RequestBody final UserSignInRequestDto requestDto) {
         final UserAuthResponseDto responseDto = authService.signIn(requestDto, authToken, fcmToken);
         return SuccessResponse.ok(responseDto);
+    }
+    @GetMapping("/countries")
+    public ResponseEntity<SuccessResponse<?>> getCountries(@RequestBody CountryCalloutRequestDto requestDto) {
+        List<String> countryNames = countryDataService.listOfCountries(requestDto.getPage(), requestDto.getPerPage(), requestDto.getLanguage());
+        return SuccessResponse.ok(countryNames);
     }
 
 }
