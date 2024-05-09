cde16c2fa1cfdae091c2c5b57bd41f6fa3f92f5f
Bae Hyeonseo <haulqogustj@naver.com>
2024-05-10 03:27:00 +0900

    [Feat] #45 Main 싱크 상세정보 그래프 api controller

--- Main/src/main/java/com/kusitms29/backendH/domain/sync/application/controller/SyncDetailController.java
+++ Main/src/main/java/com/kusitms29/backendH/domain/sync/application/controller/SyncDetailController.java
@@ -5,10 +5,7 @@ import com.kusitms29.backendH.domain.sync.application.service.SyncDetailService;
 import com.kusitms29.backendH.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/sync/detail")
@@ -19,5 +16,10 @@ public class SyncDetailController {
     public ResponseEntity<SuccessResponse<?>> syncDetail(@RequestParam(name = "syncId") Long syncId){
         SyncDetailResponseDto syncDetailResponseDto = syncDetailService.getSyncDetail(syncId);
         return SuccessResponse.ok(syncDetailResponseDto);
+    }
+    @GetMapping("/{graph}")
+    public ResponseEntity<SuccessResponse<?>> syncDetailGraph(@RequestParam(name = "syncId") Long syncId, @PathVariable(name = "graph") String graph){
+        SyncGraphResponseDto syncGraphResponseDto = syncDetailService.getSyncDetailGraph(syncId, graph);
+        return SuccessResponse.ok(syncGraphResponseDto);
     }
 }
