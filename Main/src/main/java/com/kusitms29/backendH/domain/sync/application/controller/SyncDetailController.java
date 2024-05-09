095ede2a4abc21d9b43e9c18c79749d19e130bef
Bae Hyeonseo <haulqogustj@naver.com>
2024-05-10 03:01:36 +0900

    [Feat] #45 Main 싱크 상세정보 api controller

---
+++ Main/src/main/java/com/kusitms29/backendH/domain/sync/application/controller/SyncDetailController.java
@@ -0,0 +1,21 @@
+package com.kusitms29.backendH.domain.sync.application.controller;
+
+import com.kusitms29.backendH.global.common.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/sync/detail")
+@RestController
+public class SyncDetailController {
+    private final SyncDetailService syncDetailService;
+    @GetMapping
+    public ResponseEntity<SuccessResponse<?>> syncDetail(@RequestParam(name = "syncId") Long syncId){
+        SyncDetailResponseDto syncDetailResponseDto = syncDetailService.getSyncDetail(syncId);
+        return SuccessResponse.ok(syncDetailResponseDto);
+    }
+}
