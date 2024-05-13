ebbd275670d4218e04c72e0a09778a7904e817b4
spacewalk00 <ju001217@gmail.com>
2024-05-14 01:41:20 +0900

    [Feat] #49 Main 게시글 검색 api

--- Main/src/main/java/com/kusitms29/backendH/domain/post/application/controller/dto/PostController.java
+++ Main/src/main/java/com/kusitms29/backendH/domain/post/application/controller/dto/PostController.java
@@ -4,6 +4,8 @@ import com.kusitms29.backendH.domain.post.application.controller.dto.request.Pos
 import com.kusitms29.backendH.domain.post.application.controller.dto.response.PostCreateResponseDto;
 import com.kusitms29.backendH.domain.post.application.controller.dto.response.PostDetailResponseDto;
 import com.kusitms29.backendH.domain.post.application.controller.dto.response.PostResponseDto;
+import com.kusitms29.backendH.domain.post.application.controller.dto.response.PostSearchResponseDto;
+import com.kusitms29.backendH.domain.post.application.service.PostSearchService;
 import com.kusitms29.backendH.domain.post.application.service.PostService;
 import com.kusitms29.backendH.global.common.SuccessResponse;
 import com.kusitms29.backendH.infra.config.auth.UserId;
@@ -21,6 +23,8 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final PostSearchService postSearchService;
+
     @GetMapping
     public ResponseEntity<SuccessResponse<?>> getPagingPostByPostType(@UserId Long userId, @RequestParam String postType, Pageable pageable) {
         List<PostResponseDto> responseDto = postService.getPagingPostByPostType(userId, postType, pageable);
@@ -39,6 +43,12 @@ public class PostController {
                                                          @RequestPart PostCreateRequestDto requestDto) {
         PostCreateResponseDto responseDto = postService.createPost(userId, images, requestDto);
         return SuccessResponse.ok(responseDto);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<SuccessResponse<?>> searchPost(@UserId Long userId, @RequestParam String keyword, Pageable pageable) {
+        List<PostSearchResponseDto> responseDtos = postSearchService.searchPosts(userId, keyword, pageable);
+        return SuccessResponse.ok(responseDtos);
     }
 
 }
--- Main/src/main/java/com/kusitms29/backendH/domain/post/repository/PostPagingRepository.java
+++ Main/src/main/java/com/kusitms29/backendH/domain/post/repository/PostPagingRepository.java
@@ -4,11 +4,16 @@ import com.kusitms29.backendH.domain.post.domain.Post;
 import com.kusitms29.backendH.domain.post.domain.PostType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostPagingRepository extends PagingAndSortingRepository<Post, Long> {
     Page<Post> findByPostType(PostType postType, Pageable pageable);
+
+    @Query("SELECT p FROM Post p WHERE p.title LIKE %:keyword% OR p.content LIKE %:keyword%")
+    Page<Post> searchByTitleOrContent(@Param("keyword") String keyword, Pageable pageable);
 }
 
