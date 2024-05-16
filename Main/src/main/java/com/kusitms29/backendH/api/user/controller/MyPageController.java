a73d9b0fd0ca6ec9f2b214e3426c939f4de14fa8
Bae Hyeonseo <haulqogustj@naver.com>
2024-05-16 14:27:38 +0900

    [feat]: Main #66 내가 만든 싱크 api 컨트롤러

---
+++ Main/src/main/java/com/kusitms29/backendH/api/user/controller/MyPageController.java
@@ -0,0 +1,23 @@
+package com.kusitms29.backendH.api.user.controller;
+
+import com.kusitms29.backendH.api.sync.service.dto.response.SyncInfoResponseDto;
+import com.kusitms29.backendH.api.user.service.MyPageService;
+import com.kusitms29.backendH.global.common.SuccessResponse;
+import com.kusitms29.backendH.infra.config.auth.UserId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/mypage")
+public class MyPageController {
+    private final MyPageService myPageService;
+    @GetMapping("/mysync")
+    public ResponseEntity<SuccessResponse<?>> getMySyncList(@UserId Long userId,@RequestParam(name = "take",defaultValue = "0") int take) {
+        List< SyncInfoResponseDto> syncInfoResponseDtos = myPageService.getMySyncList(userId,take);
+        return SuccessResponse.ok(syncInfoResponseDtos);
+    }
+}
---
+++ Main/src/main/java/com/kusitms29/backendH/api/user/service/MyPageService.java
@@ -0,0 +1,4 @@
+package com.kusitms29.backendH.api.user.service;
+
+public class MyPageService {
+}
