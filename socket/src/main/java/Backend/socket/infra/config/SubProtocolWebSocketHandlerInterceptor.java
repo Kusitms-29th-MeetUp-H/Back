7dd4e373d9be8196c806166e9cefe09c85631133
Bae Hyeonseo <haulqogustj@naver.com>
2024-05-20 14:46:23 +0900

    [feat]:Socket 이미지 보내기 테스트

--- socket/src/main/java/Backend/socket/infra/config/SubProtocolWebSocketHandlerInterceptor.java
+++ socket/src/main/java/Backend/socket/infra/config/SubProtocolWebSocketHandlerInterceptor.java
@@ -9,7 +9,7 @@ public class SubProtocolWebSocketHandlerInterceptor extends DelegatingIntroducti
     protected Object doProceed(MethodInvocation mi) throws Throwable {
         if (mi.getMethod().getName().equals("afterConnectionEstablished")) {
             WebSocketSession session = (WebSocketSession) mi.getArguments()[0];
-            session.setTextMessageSizeLimit(50 * 1024 * 1024); // 50MB
+            session.setTextMessageSizeLimit(50 * 1024 * 1024);
         }
         return super.doProceed(mi);
     }
