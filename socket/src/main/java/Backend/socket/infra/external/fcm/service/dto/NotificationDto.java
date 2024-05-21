7214441ee5ba2d740dc58f1cedc0c84363a839e8
spacewalk00 <ju001217@gmail.com>
2024-05-21 22:02:33 +0900

    [chore]: Main #70 주석 제거 및 Main  CI/CD

--- socket/src/main/java/Backend/socket/infra/external/fcm/service/dto/NotificationDto.java
+++ socket/src/main/java/Backend/socket/infra/external/fcm/service/dto/NotificationDto.java
@@ -18,9 +18,9 @@ public class NotificationDto {
      * 커뮤니티 : 글이름 + 댓글단이 -> 글 Id, 댓글 Id
      * 일정 : 유저이름 + 싱크이름 -> 싱크 Id
      * 채팅방 개설 공지 : 싱크이름 -> 채팅방 Id
+     * 채팅 : 채팅내용 -> 채팅방 Id
      *
      * TODO
-     * 채팅 : 채팅내용 -> 채팅방 Id
      * 후기 : 유저이름 -> 마이페이지?
      */
     private MessageTemplate template;
@@ -28,16 +28,6 @@ public class NotificationDto {
     private String infoId2;
     private String channelId;
 
-    /**
-     *  user.getId(),
-     *                     room.getRoomName(),
-     *                     user.getUserName(),
-     *                     MessageTemplate.CHAT,
-     *                     chatMessage.getContent(),
-     *                     room.getRoomSession(),
-     *                     ""
-     * @return
-     */
     public static NotificationDto getChatMessageAlarm(Long userId, String roomName, Long fromUserId,
                                                       MessageTemplate template,
                                                       String roomSessionId,
@@ -53,43 +43,4 @@ public class NotificationDto {
                 .build();
     }
 
-    public static NotificationDto getSyncReminderAlarm(Long userId, String userName, String syncName,
-                                                       MessageTemplate template,
-                                                       Long syncId) {
-        return NotificationDto.builder()
-                .id(userId.toString())
-                .str1(userName)
-                .str2(syncName)
-                .template(template)
-                .infoId(syncId.toString())
-                .channelId("RemindChannel")
-                .build();
-    }
-
-    public static NotificationDto getCommunityAlarm(Long userId, String postName, String userName,
-                                                    MessageTemplate template,
-                                                    Long postId, Long commendId) {
-        return NotificationDto.builder()
-                .id(userId.toString())
-                .str1(postName)
-                .str2(userName)
-                .template(template)
-                .infoId(postId.toString())
-                .infoId2(commendId.toString())
-                .channelId("CommunityChannel")
-                .build();
-    }
-
-    public static NotificationDto getChatRoomNoticeAlarm(Long userId, String syncName,
-                                                         MessageTemplate template,
-                                                         String roomName) {
-
-        return NotificationDto.builder()
-                .id(userId.toString())
-                .str1(syncName)
-                .template(template)
-                .infoId(roomName)
-                .channelId("OpenChatChannel")
-                .build();
-    }
 }
