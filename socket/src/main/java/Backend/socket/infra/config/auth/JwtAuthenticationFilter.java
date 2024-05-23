6904ffe41f57a17d79bc9866d63b3a6e97411be1
Bae Hyeonseo <haulqogustj@naver.com>
2024-05-23 20:55:28 +0900

    [refactor]:Socket sercurity 수정

--- socket/src/main/java/Backend/socket/infra/config/auth/JwtAuthenticationFilter.java
+++ socket/src/main/java/Backend/socket/infra/config/auth/JwtAuthenticationFilter.java
@@ -24,51 +24,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final String AUTHORIZATION = "Authorization";
     private static final String BEARER = "Bearer ";
     private final JwtProvider jwtProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
-        //url이 통과해도되는지 체크 (http 메서드와 함께 자료구조에 저장하여 검증)
-        String url = request.getRequestURI();
-        String method = request.getMethod();
-
-        if(!IgnorePathConsts.isIgnorablePath(url, HttpMethod.valueOf(method))){
-//            //위가 통과되면 토큰을 검출
-//            String authorization = request.getHeader("Authorization");
-//
-//            /*//Authorization 헤더 검증
-//            if (authorization == null || !authorization.startsWith("Bearer ")) {
-//
-//                System.out.println("token null");
-//                filterChain.doFilter(request, response); //doFilter를 통해 request와 response를
-//
-//            }*/
-//
-//            System.out.println("authorization now");
-//            //Bearer 부분 제거 후 순수 토큰만 획득
-//            String token = authorization.split(" ")[1];
-//
-//            //토큰이 유효한지 검증, 유효성 검증은 extract 메서드에서 처리
-//            Long userId = jwtUtil.extractUserClaim(token).getUserId();
-//
-//            Authentication authToken = new JwtAuthentication(userId);
-//
-//            //세션에 사용자 등록
-//            SecurityContextHolder.getContext().setAuthentication(authToken);
-        }
-
-
-
+        final String accessToken = getAccessTokenFromHttpServletRequest(request);
+        jwtProvider.validateAccessToken(accessToken);
+        final Long userId = jwtProvider.getSubject(accessToken);
+        setAuthentication(request, userId);
         filterChain.doFilter(request, response);
     }
-
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        final String accessToken = getAccessTokenFromHttpServletRequest(request);
-//        jwtProvider.validateAccessToken(accessToken);
-//        final Long userId = jwtProvider.getSubject(accessToken);
-//        setAuthentication(request, userId);
-//        filterChain.doFilter(request, response);
-//    }
 
 
     private String getAccessTokenFromHttpServletRequest(HttpServletRequest request) {
